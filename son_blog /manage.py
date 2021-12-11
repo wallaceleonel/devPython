5884ff336e748517381fa560283488d17394709e
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-12-10 23:51:50 -0300

    Update manage.py

--- son_blog /manage.py
+++ son_blog /manage.py
@@ -18,6 +18,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             
+            
             "available on your PYTHONPATH environment variable? Did you "
             
             "forget to activate a virtual environment?"
