978f3a0c748e59a638450380cc75eea3b942d7fc
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-12-10 23:51:43 -0300

    Update manage.py

--- son_blog /manage.py
+++ son_blog /manage.py
@@ -11,8 +11,10 @@ def main():
     
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'son_blog.settings')
     try:
+        
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             
