7a1e3033d0e8f42ff85c4dc8f2d53ab134cf09f6
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-12-10 23:52:06 -0300

    Update manage.py

--- son_blog /manage.py
+++ son_blog /manage.py
@@ -8,7 +8,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'son_blog.settings')
     try:
         
@@ -16,9 +15,7 @@ def main():
     except ImportError as exc:
         
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            
-            
+            "Couldn't import Django. Are you sure it's installed and "           
             "available on your PYTHONPATH environment variable? Did you "
             
             "forget to activate a virtual environment?"
