ccb4d79eae8258708b70f9984bd78e8fe2699c88
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-12-11 23:10:36 -0300

    dibilowas

--- son_blog/blog/migrations/0002_auto_20210911_1535.py
+++ son_blog/blog/migrations/0002_auto_20210911_1535.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ('blog', '0001_initial'),
     ]
-
     operations = [
         migrations.RemoveField(
             model_name='post',
