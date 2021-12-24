930b03f8c1981b9334f30b434b4ce60b98b0142e
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-12-24 00:04:07 -0300

    Update app.py

--- TinderBot /app.py
+++ TinderBot /app.py
@@ -64,7 +64,8 @@ class TinderBot:
                     sleep(3)
                     fechar_janela_match.click()
                     sleep(3)
-            except:
+           
+        except:
                 pass
 bot = TinderBot()
 bot.login()
@@ -72,6 +73,7 @@ bot.logar_com_telefone()
 bot.esperar_input_manual()
 bot.fechar_notificao_de_localizacao()
 bot.fechar_notificao_de_notificacoes()
+
 
 while True:
     bot.dar_like()
