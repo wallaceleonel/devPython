928f55745497a764b837014dbf1edb14897705ea
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2022-02-03 18:58:12 -0300

    Update urls.py

--- son_blog/son_blog/urls.py
+++ son_blog/son_blog/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from blog import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
