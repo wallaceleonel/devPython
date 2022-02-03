2ed945a7a429221a3d4b33d8f9ca0a394f499952
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2022-02-03 18:59:44 -0300

    Update views.py

--- son_blog/blog/views.py
+++ son_blog/blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 from blog.models import Post
+
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'home.html', {'posts': posts})
+
 
 def post(request,post_id):
     post = Post.objects.get(pk=post_id)
