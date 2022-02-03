403a946792b1e7aa45bb72db48aa13186e2674f7
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2022-02-03 18:57:45 -0300

    Update urls.py

--- son_blog/son_blog/urls.py
+++ son_blog/son_blog/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf.urls.static import static
 from django.conf import settings
 from blog import views
